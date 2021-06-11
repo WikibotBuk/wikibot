dbbc4c853b6e520640f64e8005547a8f40db6e1d
edgar <eguzman@buk.cl>
2021-06-11 12:52:44 -0400

    hotfix: tratar de arreglar heroku

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.autoloader = :classic
+
 module Wikibot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

200ca8584b419e0b630f4312ac35a96897f3d455
jose <jhidalgo@buk.cl>
2021-06-11 16:13:04 -0400

    feat: create command

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -2,7 +2,23 @@ require 'slack-ruby-bot'
 
 class WikiBot < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
+    client.say(text: 'pong (jose h)', channel: data.channel)
+  end
+
+  command 'crear' do |client, data, match|
+    text = data.text.partition("crear").last.strip
+    question = text[/#{"#PREGUNTA#"}(.*?)#{"#RESPUESTA#"}/m, 1].strip
+    answer = text[/#{"#RESPUESTA#"}(.*?)#{"#CATEGORÍA#"}/m, 1].strip
+    kind = text[/#{"#CATEGORÍA#"}(.*?)#{"#SUB-CATEGORÍA#"}/m, 1].strip
+    subtype = text.partition("#SUB-CATEGORÍA#").last.strip
+
+    new_wiki = Wiki.create(question: question, answer: answer, kind: kind, subtype: subtype)
+    if new_wiki.save!
+      texto = "Creado!!! question: #{question}, answer: #{answer}, kind: #{kind}, subtype: #{subtype}"
+      client.say(text: texto, channel: data.channel)
+    else
+      client.say(text: "OH NO! algo salio mal revisa que la estrucutra sea: \n#PREGUNTA# esta es la pregunta\n#RESPUESTA# esta es la respuesta\n#CATEGORÍA# tipo\n#SUB-CATEGORÍA# subtipo", channel: data.channel)
+    end
   end
 
   Wiki.all.each do |wiki|
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,6 @@
-class Wiki < ApplicationRecord
-  
+class Wiki <  ActiveRecord::Base
+    validates :question, presence: true, uniqueness: true
+    validates :answer, presence: true
+    validates :kind, presence: true
+
 end
